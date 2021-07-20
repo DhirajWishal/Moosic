2620454b8fc17e6ce4cce9e62a31ac11bcd1bccd
Fmfizzy <fizzypop470@gmail.com>
2021-07-20 12:40:46 +0530

    Changed navbar from class to id and added mirans about page

--- Scripts/navbar.js
+++ Scripts/navbar.js
@@ -1,3 +1,3 @@
-document.getElementById('navbar').innerHTML = "<div class='navbar'><div class='logo'><img src='../Assets/Moosic.png' width='100' height='50' /></div><ul class='linkalign'><li><a href='MainPage.html'>Home</a></li><li><a href='CW.html'>Products</a></li><li><a href='query.html'>Query</a></li><li><a href='QuizPage.html'>Quiz</a></li><li><a>Sitemap</a></li><li><a>About</a></li></ul></div>";
+document.getElementById('navbar').innerHTML = "<div class='logo'><img src='../Assets/Moosic.png' width='100' height='50' /></div><ul class='linkalign'><li><a href='MainPage.html'>Home</a></li><li><a href='CW.html'>Products</a></li><li><a href='query.html'>Query</a></li><li><a href='QuizPage.html'>Quiz</a></li><li><a>Sitemap</a></li><li><a href='About.html'>About</a></li></ul>";
 
 
