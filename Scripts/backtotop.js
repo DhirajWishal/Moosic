1a2fdad028539810f7a83e057e483cf9c9fd1185
Fmfizzy <fizzypop470@gmail.com>
2021-07-21 13:38:57 +0530

    added a back to top button for main page

---
+++ Scripts/backtotop.js
@@ -0,0 +1,14 @@
+mybutton = document.getElementById("backtotop");
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
